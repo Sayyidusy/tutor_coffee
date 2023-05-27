bbfb512b0ee3d08236cf2fcf87dba7fc8e7770bf
Sayyidusy syauqi Al Ghiffari <sayyidusyalghiffari@gmail.com>
2023-05-27 23:28:55 +0700

    "home screen + nav bottom berfungsi setengah"

--- app/src/main/java/com/example/tutorcoffee/MainActivity.kt
+++ app/src/main/java/com/example/tutorcoffee/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.tutorcoffee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tutorcoffee.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomAppBar.setOnMenuItemClickListener{
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.favorite -> replaceFragment(FavoriteFragment())
+                R.id.recipes -> replaceFragment(RecipesFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
