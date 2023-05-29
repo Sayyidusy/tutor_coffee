2d9d6da9798bdb8a6b0b15f893d140f3ddb674d6
shaka2003 <shakapratama2003@gmail.com>
2023-05-29 14:08:08 +0700

    slicing list resep

--- app/src/main/java/com/example/tutorcoffee/MainActivity.kt
+++ app/src/main/java/com/example/tutorcoffee/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.tutorcoffee
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var newRecycleView : RecyclerView
+    private lateinit var newArrayList: ArrayList<resep>
+    lateinit var iconAlatId : Array<Int>
+    lateinit var iconTimerId : Array<Int>
+    lateinit var nama : Array<String>
+    lateinit var timer : Array<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        iconAlatId = arrayOf(
+            R.drawable.vec_coffee_1,
+            R.drawable.vec_coffee_1,
+        )
+
+        iconTimerId = arrayOf(
+            R.drawable.timer,
+            R.drawable.timer,
+        )
+
+        nama = arrayOf(
+            "AeroPress basic recipe",
+            "AeroPress basic recipe",
+        )
+
+        timer = arrayOf(
+            "2:30",
+            "2:30",
+        )
+
+        newRecycleView = findViewById(R.id.recycleView)
+        newRecycleView.layoutManager = LinearLayoutManager(this)
+        newRecycleView.setHasFixedSize(true)
+
+        newArrayList = arrayListOf<resep>()
+        getUserData()
+    }
+    private fun getUserData() {
+        for (i in iconAlatId.indices){
+            val resep = resep(iconAlatId[i], iconTimerId[i], nama[i], timer[i])
+            newArrayList.add(resep)
+        }
+
+        newRecycleView.adapter = ViewAdapter(newArrayList)
     }
 }
---
+++ app/src/main/java/com/example/tutorcoffee/resep.kt
@@ -0,0 +1,8 @@
+package com.example.tutorcoffee
+
+data class resep(
+    val iconAlat: Int,
+    val iconTimer: Int,
+    val nama: String,
+    val timer: String
+)
