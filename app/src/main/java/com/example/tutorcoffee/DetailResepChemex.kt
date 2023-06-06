81e43abfb149c85288483fa056da0b3bbd878bf9
shaka pratama ichsan <shakapratama2003@gmail.com>
2023-06-06 20:22:25 +0700

    update chemex

--- app/src/main/java/com/example/tutorcoffee/DetailResepChemex.kt
+++ app/src/main/java/com/example/tutorcoffee/DetailResepChemex.kt
@@ -3,9 +3,9 @@ package com.example.tutorcoffee
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class DetailResep2 : AppCompatActivity() {
+class DetailResepChemex : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_resep2)
+        setContentView(R.layout.activity_detail_resep_chemex)
     }
 }
--- app/src/main/java/com/example/tutorcoffee/MainActivityChemex.kt
+++ app/src/main/java/com/example/tutorcoffee/MainActivityChemex.kt
@@ -3,12 +3,11 @@ package com.example.tutorcoffee
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ListView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivityChemex : AppCompatActivity() {
 
     private lateinit var newRecycleView : RecyclerView
     private lateinit var newArrayList: ArrayList<resep>
@@ -18,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var timer : Array<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_chemex)
 
         iconAlatId = arrayOf(
             R.drawable.chemex_1,
@@ -60,13 +59,13 @@ class MainActivity : AppCompatActivity() {
                // Toast.makeText(this@MainActivity,"resep no. $position", Toast.LENGTH_SHORT).show()
 
                 if(position == 0){
-                    val intent = Intent(this@MainActivity,DetailResep::class.java)
+                    val intent = Intent(this@MainActivityChemex,DetailResepChemex::class.java)
                     startActivity(intent)
                 } else if (position == 1) {
-                    val intent = Intent(this@MainActivity, DetailResep2::class.java)
+                    val intent = Intent(this@MainActivityChemex, DetailResepChemex2::class.java)
                     startActivity(intent)
                 } else {
-                    Toast.makeText(this@MainActivity,"resep no. $position", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivityChemex,"resep no. $position", Toast.LENGTH_SHORT).show()
                 }
             }
 
